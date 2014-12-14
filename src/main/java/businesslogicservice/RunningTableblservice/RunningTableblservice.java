0b4bea587b0a02cb768e46858b46a3125154356a
methyleneBlue <a3221655@sina.cn>
2014-12-14 15:47:03 +0800

    src update

--- src/main/java/businesslogicservice/RunningTableblservice/RunningTableblservice.java
+++ src/main/java/businesslogicservice/RunningTableblservice/RunningTableblservice.java
@@ -8,6 +8,6 @@ import vo.*;
 public interface RunningTableblservice {
 	public GoodsRecordVO[] getSaleTable (SaleConditionVO  vo) throws Exception ;
 	public ArrayList<ReceiptPO> getReceipt(ReceiptConditionVO vo)throws Exception;
-	public ProfitVO getProfit(TimeConditionVO vo)throws Exception;
+	public ProfitVO getProfit(ReceiptConditionVO vo)throws Exception;
 	public void CreditNote(ReceiptPO po)throws Exception;
 }
