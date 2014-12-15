2aed0098694344955de4104f5e4cb147ae4d6ec1
methyleneBlue <a3221655@sina.cn>
2014-12-15 19:42:08 +0800

    更新期初建账接口

--- src/main/java/dataservice/SetAccountdataservice/SetAccountdataservice.java
+++ src/main/java/dataservice/SetAccountdataservice/SetAccountdataservice.java
@@ -6,8 +6,13 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface SetAccountdataservice extends Remote {
-	public ResultMessage setup(String[] name) throws RemoteException;
+	public ResultMessage setup(String name) throws RemoteException;
 	public ResultMessage goods() throws RemoteException;
 	public ResultMessage bank() throws RemoteException;
 	public ResultMessage client() throws RemoteException;
+	public ResultMessage setDefaultAccount(String name) throws RemoteException;
+	public ResultMessage getDefaultAccount() throws RemoteException;
+	public ResultMessage getAllAccount() throws RemoteException;
+	public ResultMessage inherit(String name)throws RemoteException;
+	public ResultMessage creatCopy()throws RemoteException;
 }
